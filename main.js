c8e32e02f05aa843e44130ea92437217fc4a7e20
zhouchenyang <278938294@qq.com>
2022-09-09 14:01:32 +0800

    swiper

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+$http.afterRequest = function(options){
+  uni.hideLoading()
+}
+
+uni.$showMsg = function(title='数据请求失败',duration=1500){
+   uni.showToast({
+      title,
+      duration,
+      icon:'none'
+   })
+}
 
 // #ifndef VUE3
 import Vue from 'vue'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <view>
-    home
+    <swiper :indicator-dots="true" :autoplay="true" :circular="true" :duration="1000" :interval="2000">
+      <navigator class="swiper-item"  v-for="item in swiperList" key="item.goods_id">
+        <swiper-item>
+          <image :src="item.image_src"></image>
+        </swiper-item>
+      </navigator>
+    </swiper>
   </view>
 </template>
 
@@ -8,12 +14,35 @@
   export default {
     data() {
       return {
-        
+        swiperList: []
       };
+    },
+    methods: {
+      async getSwiperList() {
+        const {
+          data
+        } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        if (data.meta.status !== 200) {
+          return uni.$showMsg
+        }
+        this.swiperList = data.message
+      }
+    },
+    onLoad() {
+      this.getSwiperList()
     }
   }
 </script>
 
 <style lang="scss">
-
+  swiper {
+    height: 330rpx;
+  
+  .swiper-item {
+      image {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 </style>
